70263f942b42ccd2778f371db960436ed351bea3
AndreaSenac <andrea.cfporto@gmail.com>
2014-03-11 20:37:50 -0300

    Hello You

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package senac_alproii;
+
+/**
+ *
+ * @author Andrea Fofonka
+ */
+
+public class HelloWorld {
+    
+    public static void main (String args[]){
+        System.out.print ("Hello World!! ^^ ");
+    }
+    
+}
